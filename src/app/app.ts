e54b0a653a9fff412e64561bad5baff9d8e19347
Dayana Escobar <oe21074@ucad.edu.sv>
2025-08-20 19:50:38 -0600

    Laboratorio

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Catalogo } from './catalogo/catalogo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Catalogo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('marjorie_ortiz');
+  title = signal('marjorie_ortiz');
 }
---
+++ src/app/catalogo/catalogo.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo',
+  templateUrl: './catalogo.html',
+  styleUrl: './catalogo.css'
+})
+export class Catalogo {
+  companero1: string = 'Gerson Eliezer Zuniga Ruiz';
+  companero2: string = 'Marjorie Dayana Ortiz Escobar';
+  companero3: string = 'Emilio Rigoberto Hernandez';
+ 
+}
+
